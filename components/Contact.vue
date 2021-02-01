617bdcf3cb38acb6cf1fbb76ab1d69506375c12d
Praxis <hello@vulpinecat.com>
2021-02-01 11:48:52 +0100

    Create landing page components

---
+++ components/Contact.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-card id="contact-form">
+        <v-card-title v-if="showFront" class="headline">
+          Contact Form
+        </v-card-title>
+      </v-card>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                showFront: true
+            }
+        },
+        methods: {
+            scrollToForm() {
+                const el = document.getElementById("contact-form")
+
+                if(el) {
+                  el.scrollIntoView({behavior: 'smooth'})
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/WorkHistory.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-timeline>
+        <pre>{{history}}</pre>
+    </v-timeline>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                history: [
+                    {
+                        employer: "Avacon AG",
+                        startDate: new Date(2016, 4),
+                        endDate: new Date(2017, 7),
+                        location: "Salzgitter",
+                        companyProfile: "https://www.linkedin.com/company/avaconag/",
+                        attachment: "DarianReck_LetterOfRecommendations_Avacon_GER.pdf"                           
+                    },
+                    {},
+                    {},
+                    {}
+                ]
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
